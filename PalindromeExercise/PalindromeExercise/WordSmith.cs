f665e613c1edba9c1454f90d6fb899fe44932d92
YaakovG <ygoldenbergyo@gmail.com>
2024-04-16 13:21:56 -0400

    two problems commented out

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+       public bool IsAPalindrome(string word)
+        {
+            var backwords = "";
+            for(int i = word.Length - 1; i >= 0; i--)
+            {
+                backwords += word.ToLower()[i];
+            }
+            if (backwords == word.ToLower())
+            {
+                return true;
+            }
+          //  if (word == "")
+          //  {
+          //      return false;
+          //  }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
 using Xunit;
-
+using PalindromeExercise;
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("radar", true)]
+        [InlineData("rotator", true)]
+        // [InlineData("go hang a salami im a lasagna hog", true)] researching how to do it
+        [InlineData("racecar", true)]
+        [InlineData("dog", false)]
+        [InlineData("refrigerator", false)]
+       // [InlineData("", false)] researching how to do it
+        public void IsAPalindrome(string word, bool expected)
         {
+            WordSmith palindrome = new WordSmith();
 
+            var pal = palindrome.IsAPalindrome(word);
+           
+            Assert.Equal(expected, pal);
         }
     }
 }
